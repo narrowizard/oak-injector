ab1a71e7f271e85de395ff92724e245b1efc23aa
narro <501xs05@163.com>
2020-10-10 14:52:41 +0800

    fix: dependency

--- deps.ts
+++ deps.ts
@@ -4,7 +4,7 @@ export {
   Router,
   RouterContext,
 } from "https://deno.land/x/oak/mod.ts";
-export { ServerRequest } from "https://deno.land/std@0.69.0/http/server.ts";
+export { ServerRequest } from "https://deno.land/std@0.73.0/http/server.ts";
 export { format } from "https://deno.land/std/datetime/mod.ts";
 export * as log from "https://deno.land/std/log/mod.ts";
-export { assertEquals } from "https://deno.land/std@0.69.0/testing/asserts.ts";
+export { assertEquals } from "https://deno.land/std@0.73.0/testing/asserts.ts";
